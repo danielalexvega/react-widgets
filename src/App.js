95596ce1903d609dd9dcc13561a06620ddb7796e
Daniel Vega <danielalexvega@gmail.com>
2020-09-18 15:58:52 -0500

    added Accordion.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: "Who's the best NBA player in the game today?",
+    content: 'Lebron James, the 16-time all-NBA 4-time MVP 3-time Champ.'
+  }
+
+];
+
 export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items}/>
     </div>
   )
 }
