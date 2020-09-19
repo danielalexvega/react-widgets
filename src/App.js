6712054afaec6a55449fcf44b50bf86682f482a0
Daniel Vega <danielalexvega@gmail.com>
2020-09-19 13:56:50 -0500

    started the search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers'
-  },
-  {
-    title: "Who's the best NBA player in the game today?",
-    content: 'Lebron James, the 16-time all-NBA 4-time MVP 3-time Champ.'
-  }
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library among engineers'
+//   },
+//   {
+//     title: "Who's the best NBA player in the game today?",
+//     content: 'Lebron James, the 16-time all-NBA 4-time MVP 3-time Champ.'
+//   }
 
-];
+// ];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items}/>
+      {/* <Accordion items={items}/> */}
+      <Search />
     </div>
   )
 }
