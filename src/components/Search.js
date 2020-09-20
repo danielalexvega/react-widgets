c6bddb49fe27a8c42fdd9fb27b59f28f59563b7e
Daniel Vega <danielalexvega@gmail.com>
2020-09-20 14:07:52 -0500

    finished search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,8 +19,41 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    searchWikipedia();
+    if (term && !results.length) {
+      searchWikipedia();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          searchWikipedia();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
+
+  const renderedResults = results.map(result => {
+    return (
+      <div className='item' key={result.pageid}>
+        <div className='right floated content'>
+          <a
+            className='ui button'
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className='content'>
+          <div className='header'>
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -33,6 +66,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className='ui celled list'>
+        {renderedResults}
       </div>
     </div>
   );
