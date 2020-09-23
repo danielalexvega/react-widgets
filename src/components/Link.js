321b6e92ea02e96476535f9efe5debde23c5c515
Daniel Vega <danielalexvega@gmail.com>
2020-09-22 23:54:19 -0500

    handle command clicks in link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Link = ({ href, className, children }) => {
   const onClick = (event) => {
+    if(event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     window.history.pushState({}, '', href);
-    
+
     const navEvent = new PopStateEvent('popstate');
     window.dispatchEvent(navEvent);
   };
